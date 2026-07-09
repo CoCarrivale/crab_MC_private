5f1c54da0d931baa4fcf8d88d0e38a0d4922f077
Francesca Fiore <francesca.fiore@cern.ch>
2026-07-09 18:32:13 +0200

    Step chains updated for 2023

---
+++ runners/2023LHEGS_SMP/chain_step_3_test.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+#set -x
+
+SEED=$1
+
+RUN_DIR=${PWD}
+echo ">> Setting RUN_DIR to ${RUN_DIR}"
+
+CMSSW_RELEASE=CMSSW_13_0_14
+SCRAM_ARCH=el8_amd64_gcc12
+
+if [ "${CMSSW_RELEASE}" != "local" ]; then
+    if [ -d ${CMSSW_RELEASE} ]; then
+      echo ">> Cleaning up existing ${CMSSW_RELEASE} directory"
+      rm -r ${CMSSW_RELEASE}
+    fi
+    echo ">> Setting up release area for ${CMSSW_RELEASE} and arch ${SCRAM_ARCH}"
+    if [ ! -d ${CMSSW_RELEASE} ]; then
+      scram project CMSSW ${CMSSW_RELEASE}
+    fi
+
+    cd ${CMSSW_RELEASE}/src
+    eval `scramv1 runtime -sh`
+    cd -
+
+fi
+
+python ${RUN_DIR}/modifyCfg.py ${RUN_DIR}/RECO_step.py ${RUN_DIR}/step_3_cfg.py --randomSeeds=${SEED}
+
+cmsRun -e -j FrameworkJobReport.xml ${RUN_DIR}/step_3_cfg.py
